6295946917807c659e1be8e0a9d4ddb436e92dfc
Priyankavhiremath <priyankahiremath221b@gmail.com>
2021-03-02 18:12:30 +0100

    created database

--- index.js
+++ index.js
@@ -1,37 +1,68 @@
+require("dotenv").config();
 const express = require ('express')
-const contentful = require ('./Router/contentful')
-
 const app = express();
+
+const {Pool} = require("pg")
+
 app.use(express.json())
 
-const PORT = process.env.PORT || 4000
+const pool = new Pool()
+
+
+const PORT = process.env.PORT || 3000
 
 app.get('/', (req, res)=> {
     res.send('Hi There')
 })
 
-// GET multiple recipes
- app.get('/', )
+app.get('/api/recipes', (req, res) =>{
+    pool
+        .query("Select * FROM recipes")
+        .then((data) => res.json(data.rows))
+        .catch((err) => res.sendStatus(500));
+} )
 
-// GET single recipes
-app.get('/api/recipes',)
-
-// POST recipes
-app.post('/api/recipes', (req,res)=>{
-
+app.post('/seed', async (req, res) => {
+    const seedTable = `
+    CREATE TABLE recipes(
+        id SERIAL PRIMARY KEY,
+        title varchar(100), 
+        description varchar(255),
+        ingredients varchar(255),
+        directions text,
+        mealtype varchar(255),
+        image varchar(100),
+        cook_time int,
+        prep_time int
+        
+    );
+    `
+    try {
+        const response = await pool.query(seedTable)
+        res.json(response)
+    } catch (e) {
+        res.status(500).send(e.message)
+    }
 })
 
-// PUT recipes
-app.put('/api/recipes', (req, res)=> {
-
+app.delete('/destroy', async (req, res) => {
+    const destroyTable = `DROP TABLE recipes;`
+    try {
+        const response = await pool.query(destroyTable)
+        res.json(response)
+    } catch (e) {
+        res.status(500).send(e.message)
+    }
 })
 
-// DELETE recipes
-app.delete('/api/recipes', (req, res)=>{
 
+app.get('/time', (req,res)=>{
+    pool.query("SELECT now();")
+    .then((data) => res.json(data.rows))
+        .catch((err) => res.sendStatus(500));
 })
 
-app.use ('/api/recipes', contentful)
+// app.use ('/api/recipes', contentful)
 
 app.listen(PORT, () =>console.log(`Server is running at Port ${PORT}`) )
 
