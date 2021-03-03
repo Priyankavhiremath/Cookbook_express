21515d0042317c4cea6cbc98d270a375e8bf5e07
Priyankavhiremath <priyankahiremath221b@gmail.com>
2021-03-03 17:13:59 +0100

    feature-contentful

---
+++ Router/contentful.js
@@ -0,0 +1,80 @@
+const express = require('express')
+const Router = express.Router()
+
+const { Pool } = require("pg")
+const pool = new Pool()
+
+// GET multiple recipes *******************************************
+Router.get('/', (req, res) =>{
+    pool
+        .query("Select * FROM Recipes")
+        .then((data) => res.json(data.rows))
+        .catch((err) => res.sendStatus(500));
+} )
+
+//////get one recipe *********************************************
+Router.get("/:id", (req, res)=>{
+    const {id} = req.params;
+    pool
+        .query("SELECT * FROM recipes WHERE id=$1", [id] )
+        .then(data => res.json(data.rows))
+        .catch(err => res.sendStatus(500))
+})
+
+
+//////delete one recipe ********************************************
+
+Router.delete('/:id' , (req,res)=>{
+    const {id} = req.params;
+    const deleteOne={
+        text:`DELETE FROM recipes WHERE id=$1 RETURNING *`,
+        values: [id]
+    }
+
+    pool
+    .query(deleteOne)
+    .then((data)=>res.status(201).json(data.rows))
+    .catch((err)=>res.sendStatus(500))
+
+})
+//////Post one recipe TIM ************************************************
+  Router.post("/", (req, res) => {
+    // console.log(req.body)
+    const { title, description, ingredients, directions, mealtype, image, cook_time, prep_time, } = req.body;
+
+  const createOneRecipes = {
+    text: `
+    INSERT INTO recipes ( title, description, ingredients, directions, mealtype, image, cook_time,prep_time)
+    VALUES($1, $2, $3, $4, $5, $6, $7, $8)
+    RETURNING *
+    `,
+    values: [title,description,ingredients,directions, mealtype, image, cook_time, prep_time,]
+}
+     pool
+        .query(createOneRecipes)
+        .then((data) => res.status(201).json(data.rows))
+        .catch((err) => res.sendStatus(500));
+});
+
+//////put one recipe priyanka ************************************************************************
+Router.put('/:id', (req, res) =>{
+    const {id} =req.params
+    const {title, description, ingredients, directions, mealtype, image, cook_time, prep_time} = req.body
+    
+    const editRecipe = {
+        text:`
+        UPDATE recipes
+        SET title=$1, description=$2 ,ingredients=$3 , directions=$4 , mealtype=$5 , image=$6 , cook_time=$7 , prep_time=$8 
+        WHERE id=$9
+        RETURNING *
+        `,
+        values: [title, description, ingredients, directions, mealtype, image, cook_time, prep_time,id]
+    }
+    pool.query(editRecipe)
+        .then(data => res.status(200).json(data.rows))
+        .catch(err => res.sendStatus(500))
+})
+
+
+
+module.exports= Router
--- index.js
+++ index.js
@@ -2,69 +2,18 @@ require("dotenv").config();
 const express = require ('express')
 const app = express();
 
-const {Pool} = require("pg")
+const contentful = require("./Router/contentful");
 
 app.use(express.json())
-
-const pool = new Pool()
-
 
 const PORT = process.env.PORT || 3000
 
 app.get('/', (req, res)=> {
     res.send('Hi There')
 })
+app.use ('/api/recipes', contentful)
 
-app.get('/api/recipes', (req, res) =>{
-    pool
-        .query("Select * FROM recipes")
-        .then((data) => res.json(data.rows))
-        .catch((err) => res.sendStatus(500));
-} )
-
-app.post('/seed', async (req, res) => {
-    const seedTable = `
-    CREATE TABLE recipes(
-        id SERIAL PRIMARY KEY,
-        title varchar(100), 
-        description varchar(255),
-        ingredients varchar(255),
-        directions text,
-        mealtype varchar(255),
-        image varchar(100),
-        cook_time int,
-        prep_time int
-        
-    );
-    `
-    try {
-        const response = await pool.query(seedTable)
-        res.json(response)
-    } catch (e) {
-        res.status(500).send(e.message)
-    }
-})
-
-app.delete('/destroy', async (req, res) => {
-    const destroyTable = `DROP TABLE recipes;`
-    try {
-        const response = await pool.query(destroyTable)
-        res.json(response)
-    } catch (e) {
-        res.status(500).send(e.message)
-    }
-})
-
-
-app.get('/time', (req,res)=>{
-    pool.query("SELECT now();")
-    .then((data) => res.json(data.rows))
-        .catch((err) => res.sendStatus(500));
-})
-
-// app.use ('/api/recipes', contentful)
-
-app.listen(PORT, () =>console.log(`Server is running at Port ${PORT}`) )
+app.listen(PORT, () =>{console.log(`Server is running at Port ${PORT}`) })
 
 
 
