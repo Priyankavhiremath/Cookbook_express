ce15bd6c0657ff7ffe4be212fba4c7c95be7c097
timjoschneider <76120040+timjoschneider@users.noreply.github.com>
2021-03-09 09:00:02 +0100

    Update Footer.js

--- frontend/src/components/layout/Footer.js
+++ frontend/src/components/layout/Footer.js
@@ -4,6 +4,23 @@ import {Link} from "react-router-dom";
 const Footer = () => {
     return (
         <footer className="btn-info  text-center text-lg-start">
+        
+                    <section class="mb-4 primarycolor">
+                <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i class="fab fa-facebook-f"></i></a>
+
+                <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button" ><i class="fab fa-twitter"></i></a>
+
+                <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i class="fab fa-google"></i></a>
+
+                <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i class="fab fa-instagram"></i ></a>
+
+                <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i class="fab fa-linkedin-in"></i></a>
+
+                <a class="btn btn-outline-light btn-floating m-1" href="https://github.com/Priyankavhiremath/Cookbook_express" role="button"><i class="fab fa-github"></i></a>
+
+                <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i class="fab fa-discord"></i></a>
+            </section>
+        
             <div className="text-center p-3">
                 © 2021 Copyright:
                 <Link class="text-dark" to="https://google.com/">
